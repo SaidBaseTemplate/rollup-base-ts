ebb6bc8de47f07399d1e01c0b1c38255f5a8d8ca
guizimo <17680262548@163.com>
2024-06-12 23:46:49 +0800

    doc：调整package

---
+++ dist/index.esm.js
@@ -0,0 +1,10 @@
+function sum(a, b) {
+    return a + b;
+}
+
+console.log(sum(1, 2));
+var index = {
+    sum
+};
+
+export { index as default };
---
+++ dist/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function sum(a, b) {
+    return a + b;
+}
+
+console.log(sum(1, 2));
+var index = {
+    sum
+};
+
+module.exports = index;
